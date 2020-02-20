a839cf157e11495a3a24e2a63a861a92c255acb4
MatveevaElena <wkatara@gmail.com>
2020-02-20 09:20:34 +0400

    add News and changing news fields

---
+++ common/modules/news/views/news/_form.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+use kartik\datetime\DateTimePicker;
+
+/* @var $this yii\web\View */
+/* @var $model common\modules\news\models\News */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="news-form">
+
+    <?php $form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data', 'class' => 'model-form']]); ?>
+
+    <?= $form->field($model, 'title')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'author')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'time')-> widget(DateTimePicker::className(),[
+        'type' => DateTimePicker::TYPE_INPUT,
+        'options' => ['placeholder' => 'Ввод даты/времени...'],
+        'convertFormat' => true,
+        'pluginOptions' => [
+            'format' => 'dd.MM.yyyy H:i',
+            'autoclose' => true,
+        ]
+    ]) ?>
+
+    <?= $form->field($model, 'img')-> fileinput() ?>
+
+    <?= $form->field($model, 'short')->textInput(['maxlength' => true]) ?>
+
+    <?= $form->field($model, 'text')->widget(\common\components\widgets\Redactor::className()) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton(Yii::t('ML', $model->isNewRecord ? 'Create' : 'Update'), ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
